21b8ba4b6dab2db33c8d49534493e36251d1f921
Rizky Fajri <mansamussolini@gmail.com>
2022-04-02 16:14:07 +0700

    Create DataMahasiswa_1302204043.cs

---
+++ mod6_tugas kelompok sem4/mod6_tugas kelompok sem4/DataMahasiswa_1302204043.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace mod6_tugas_kelompok_sem4
+{
+    internal class DataMahasiswa_1302204043
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+
+
+    }
+}
