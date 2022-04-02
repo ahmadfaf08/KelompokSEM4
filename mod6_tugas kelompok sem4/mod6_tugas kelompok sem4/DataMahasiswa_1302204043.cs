8b01377c3590ef7659e2b8ea537f47c199be61cc
Rizky Fajri <mansamussolini@gmail.com>
2022-04-02 16:17:12 +0700

    Update DataMahasiswa_1302204043.cs

--- mod6_tugas kelompok sem4/mod6_tugas kelompok sem4/DataMahasiswa_1302204043.cs
+++ mod6_tugas kelompok sem4/mod6_tugas kelompok sem4/DataMahasiswa_1302204043.cs
@@ -10,9 +10,9 @@ namespace mod6_tugas_kelompok_sem4
 {
     internal class DataMahasiswa_1302204043
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Gender { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Gender { get; set; }
         public int Age { get; set; }
 
 
