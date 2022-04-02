c0b76ed572f7157697dd6d1f0a817b9d5a8dbd00
Rizky Fajri <mansamussolini@gmail.com>
2022-04-02 15:55:56 +0700

    Main
    Main created

--- mod6_tugas kelompok sem4/mod6_tugas kelompok sem4/Program.cs
+++ mod6_tugas kelompok sem4/mod6_tugas kelompok sem4/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
+﻿namespace mod6_tugas_kelompok_sem4
+{
+    public class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}
